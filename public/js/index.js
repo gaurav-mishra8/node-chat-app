1610f9db8ff7d822016a37895d5c86fb4e3204f6
gaurav1_mishra <gaurav1.mishra@paytm.com>
2017-11-25 21:09:40 +0530

    Add emitting and listening events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+var socket = io();
+socket.on('connect', function() {
+  console.log('connected to server');
+
+  // socket.emit('createEmail',{
+  //   to:'Jen@example.com',
+  //   text:'Hey this is Gaurav'
+  // });
+
+  socket.emit('createNewMessage', {
+    to: 'Joshua@example.com',
+    text: 'This will be good course'
+  });
+
+});
+
+socket.on('newMessageReceived', function(newMessage) {
+  console.log('New Message Received', newMessage);
+});
+
+socket.on('disconnect', function() {
+  console.log('disconnected from server');
+});
+
+// socket.on('newEmail', function(email) {
+//   console.log('New email', email);
+// });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
+
+  // socket.emit('newEmail', {
+  //   from: 'Mike@example.com',
+  //   text: 'Hey whats up',
+  //   createAt: 123
+  // });
+
+  socket.emit('newMessageReceived', {
+    from: 'anthony@example.com',
+    text: 'Hi from Anthony',
+    createdAt: 131323
+  });
+
+  socket.on('createNewMessage', (newMessage) => {
+    console.log('createNewMessage', newMessage);
+  });
+
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('createEmail', newEmail);
+  // });
 
   socket.on('disconnect', () => {
     console.log('disconnected from client');
