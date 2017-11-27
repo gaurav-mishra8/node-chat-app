d6225ea341fb995bf54fc51f08db4e9189285610
gaurav1_mishra <gaurav1.mishra@paytm.com>
2017-11-27 22:22:03 +0530

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,28 @@ socket.on('connect', function() {
   console.log('connected to server');
 });
 
-socket.on('newMessage', function(newMessage) {
-  console.log('New Message Received', newMessage);
+socket.on('newMessage', function(message) {
+  console.log('New Message Received', message);
+
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}:${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
   console.log('disconnected from server');
+});
+
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(data) {
+
+  });
+
 });
